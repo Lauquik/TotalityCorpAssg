ce2158b54c793d5a57a784b691a2583813a85cf2
Lauquik <laukik.22010776@viit.ac.in>
2024-05-29 12:28:49 +0530

    added server and db

---
+++ db/db.go
@@ -0,0 +1,7 @@
+package db
+
+var Users = []UserDetails{
+	{Id: "1", Name: "Steve", City: "LA", Phone: 1234567890, Height: 5.8, Married: "TRUE"},
+	{Id: "2", Name: "John", City: "NY", Phone: 2345678901, Height: 5.9, Married: "TRUE"},
+	{Id: "3", Name: "Anna", City: "SF", Phone: 3456789012, Height: 5.7, Married: "FALSE"},
+}
---
+++ db/model.go
@@ -0,0 +1,10 @@
+package db
+
+type UserDetails struct {
+	Id      string
+	Name    string
+	City    string
+	Phone   int64
+	Height  float32
+	Married string
+}
